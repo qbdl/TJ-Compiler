42dc5ef1de5496e900d78ec42242fb86fb7af9da
qbdl <1260017352@qq.com>
2023-05-10 12:01:55 +0800

    增加了+，-号的C->汇编（利用printf直接仿照汇编格式输出）

--- main.c
+++ main.c
@@ -7,9 +7,30 @@ int main(int argc,char **argv){
         return 1;
     }
 
+    char *p=argv[1];//传入的参数（sh里的input)
+
+    printf(".intel_syntax noprefix\n");
     printf("  .globl main\n");
     printf("main:\n");
-    printf("  mov $%d, %%rax\n", atoi(argv[1]));
+    printf("  mov rax,%ld\n",strtol(p,&p,10));//strtol将当前指向字符转为某个基数的整数（并移动指针），直至遇到非数值/其他非法字符
+    
+    while(*p){
+        if(*p=='+'){
+            p++;//指针手动下移
+            printf("  add rax,%ld\n",strtol(p,&p,10));
+            continue;
+        }
+        else if(*p=='-'){
+            p++;
+            printf("  sub rax,%ld\n",strtol(p,&p,10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return 1;
+    }
+    
+    
     printf("  ret\n");
     return 0;
 }
