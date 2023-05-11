26ec67ac0d4b7da9810cf757de8ddae8edf94803
qbdl <1260017352@qq.com>
2023-05-11 22:28:54 +0800

    commit5:增加了 * ， / , （）

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include<stdlib.h>
 #include<string.h>
 
+//
+// Tokenizer
+//
+
 typedef enum
 {
     TK_RESERVED, // Keywords or punctuators
@@ -108,7 +112,7 @@ Token *tokenize(void)
     }
 
     // Punctuator
-    if (*p == '+' || *p == '-') {
+    if (ispunct(*p)) {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -127,32 +131,154 @@ Token *tokenize(void)
   return head.next;
 }
 
+//
+// Parser
+//
+
+typedef enum 
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node 
+{
+  NodeKind kind; // Node kind
+  Node *lhs;     // Left-hand side
+  Node *rhs;     // Right-hand side
+  long val;      // Used if kind == ND_NUM
+};
+
+static Node *new_node(NodeKind kind)
+{
+  Node*node=calloc(1,sizeof(Node));
+  node->kind=kind;
+  return node;
+}
+
+static Node *new_binary(NodeKind kind,Node *lhs,Node *rhs)
+{
+  Node *node=new_node(kind);
+  node->lhs=lhs;
+  node->rhs=rhs;
+  return node;
+}
+
+static Node *new_num(int val)
+{
+  Node *node=new_node(ND_NUM);
+  node->val=val;
+  return node;
+}
+
+//
+static Node *expr(void);
+static Node *mul(void);
+static Node *primary(void);
+
+// expr = mul ("+" mul | "-" mul)*
+static Node *expr(void)
+{
+  Node *node=mul();
+
+  for(;;){
+    if(consume('+'))
+      node=new_binary(ND_ADD,node,mul());
+    else if(consume('-'))
+      node=new_binary(ND_SUB,node,mul());
+    else 
+      return node;
+  }
+}
+
+// mul = primary ("*" primary | "/" primary)*
+static Node *mul(void)
+{
+  Node *node=primary();
+  for(;;){
+    if(consume('*'))
+      node=new_binary(ND_MUL,node,primary());
+    else if(consume('/'))
+      node=new_binary(ND_DIV,node,primary());
+    else
+      return node;
+  }
+}
+
+// primary = "(" expr ")" | num
+static Node *primary(void)
+{
+  if(consume('(')){
+    Node *node=expr();
+    expect(')');
+    return node;
+  }
+
+  return new_num(expect_number());
+}
+
+//
+// Code generator
+//
+
+static void gen(Node *node)
+{
+  if(node->kind==ND_NUM){
+    printf("  push %ld\n",node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+  
+  switch(node->kind){
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
 
 int main(int argc,char **argv)
 {
     if(argc!=2)
         error("%s: invalid number of arguments",argv[0]);
 
+    // Tokenize and parse.
     user_input=argv[1];
     token=tokenize();
-
+    //从根节点往下进行汇编代码生成
+    Node *node=expr();
+    
     printf(".intel_syntax noprefix\n");
     printf("  .globl main\n");
     printf("main:\n");
 
-    // The first token must be a number
-    printf("  mov rax,%ld\n",expect_number());
-    
-    //subsequent "+ number" or "- number"
-    while(!at_eof()){
-        if(consume('+')){
-            printf("  add rax, %ld\n",expect_number());
-            continue;
-        }
-        expect('-');
-        printf("  sub rax, %ld\n",expect_number());
-    }
+    // Traverse the AST to emit assembly.
+    gen(node);
 
+    // A result must be at the top of the stack, so pop it
+    // to RAX to make it a program exit code.
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -23,5 +23,8 @@ assert 0 0
 assert 42 42 #digit 
 assert 21 '5+20-4' # sub,add
 assert 41 ' 12 + 34 - 5 ' # space
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
 
 echo OK
