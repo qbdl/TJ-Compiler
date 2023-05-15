07c01719353881725d41beb8c675e34e616d86a8
qbdl <1260017352@qq.com>
2023-05-15 16:05:33 +0800

    commit25：增加 取地址&与 取内容*

--- codegen.c
+++ codegen.c
@@ -6,12 +6,19 @@ static char *argreg[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
 static int labelseq=1;
 static char *funcname;
 
+static void gen(Node *node);
+
+
 // Pushes the given node's address to the stack.
 static void gen_addr(Node *node) //计算具体变量的地址
 {
-  if (node->kind == ND_VAR) {
-    printf("  lea rax,[rbp-%d]\n",node->var->offset);
+  switch (node->kind) {
+  case ND_VAR:
+    printf("  lea rax, [rbp-%d]\n", node->var->offset);
     printf("  push rax\n");
+    return;
+  case ND_DEREF:
+    gen(node->lhs);
     return;
   }
 
@@ -53,6 +60,13 @@ static void gen(Node *node)
       gen_addr(node->lhs);
       gen(node->rhs);
       store();
+      return;
+    case ND_ADDR:
+      gen_addr(node->lhs);
+      return;
+    case ND_DEREF:
+      gen(node->lhs);
+      load();
       return;
     case ND_IF:{
       int seq=labelseq++;
--- parse.c
+++ parse.c
@@ -300,7 +300,7 @@ static Node *mul(void)
   }
 }
 
-// unary = ("+" | "-")? unary
+// unary = ("+" | "-" | "*" | "&")? unary
 //       | primary
 static Node *unary(void) 
 {
@@ -309,6 +309,10 @@ static Node *unary(void)
     return unary();
   if (tok = consume("-"))
     return new_binary(ND_SUB, new_num(0, tok), unary(), tok);
+  if (tok = consume("&"))
+    return new_unary(ND_ADDR, unary(), tok);
+  if (tok = consume("*"))
+    return new_unary(ND_DEREF, unary(), tok);
   return primary();
 }
 
